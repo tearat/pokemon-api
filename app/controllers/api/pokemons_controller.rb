434f0995ef9b16e901b7ee2f549b425651640a4b
teraaret <raiiliiat@yandex.ru>
2020-11-12 21:27:42 +0500

    Controller with pagination

--- app/controllers/api/pokemons_controller.rb
+++ app/controllers/api/pokemons_controller.rb
@@ -2,43 +2,34 @@ class Api::PokemonsController < ApplicationController
   # acts_as_token_authentication_handler_for User
 
   def index
-    limit  = params[:limit]  ? params[:limit].to_i  : 10
-    offset = params[:offset] ? params[:offset].to_i : 0
+    page = params[:page] ? params[:page].to_i : 1
+    limit = params[:limit] ? params[:limit].to_i : 10
     filter = params[:filter] || nil
 
-    if filter
-      @pokemons = Pokemon.where("`name_clean` LIKE ?", "%#{filter}%").limit(limit).offset(offset).all
-    else
-      @pokemons = Pokemon.limit(limit).offset(offset).all
-    end
+    @pokemons = Pokemon.all
+    total = @pokemons.size
 
-    total = Pokemon.all.size
+    @pokemons = @pokemons.where("`name_clean` LIKE ?", "%#{filter}%") if filter
+    found = @pokemons.size
+    pages = (found.to_f / limit.to_f).ceil
 
-    is_next_page = (total - offset) >= limit
-    next_params = {
-      :limit => limit,
-      :offset => offset + limit,
-      :filter => filter,
-    }.map { |key, value| "#{key}=#{value}" }.join "&"
-    next_page = is_next_page ? "http://localhost:3000/api/pokemons?#{next_params}" : nil
+    paginated = paginate @pokemons, per_page: limit
+    count = paginated.size
 
-    is_prev_page = offset > 0
-    prev_offset = (offset - limit) >= 0 ? offset - limit : 0
-    prev_params = {
-      :limit => limit,
-      :offset => prev_offset,
-      :filter => filter,
-    }.map { |key, value| "#{key}=#{value}" }.join "&"
-    prev_page = is_prev_page ? "http://localhost:3000/api/pokemons?#{prev_params}" : nil
+    url = url_for controller: "pokemons", action: "index"
+    next_page = (found - (limit * page)) > 0 ? "#{url}?page=#{page + 1}&limit=#{limit}&filter=#{filter}" : nil
+    prev_page = (page > 1) ? "#{url}?page=#{page - 1}&limit=#{limit}&filter=#{filter}" : nil
 
     render json: {
-      "total"     => total,
-      "count"     => @pokemons.size,
-      "offset"    => offset.to_i,
-      "limit"     => limit.to_i,
+      "total" => total,
+      "found" => found,
+      "limit" => limit,
+      "count" => count,
+      "pages" => pages,
+      "current_page" => page,
       "next_page" => next_page,
       "prev_page" => prev_page,
-      "pokemons"  => @pokemons.map { |pokemon| PokemonsListSerializer.new(pokemon) }
+      "pokemons" => paginated,
     }
   end
 end
